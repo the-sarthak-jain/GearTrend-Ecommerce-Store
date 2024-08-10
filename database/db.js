1412024ba710274a1a950807f3d767bf2c217071
Sarthak Jain <112318814+the-sarthak-jain@users.noreply.github.com>
2024-08-10 13:02:29 +0530

    Initial commit

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Replace with your actual MongoDB URI
-const mongoURI = "mongodb+srv://<username>:<password>@cluster0.vzsyypy.mongodb.net/sarthakdb?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = process.env.MONGO_URI;
 
 const con = mongoose.connect(mongoURI, {
     useNewUrlParser: true,
